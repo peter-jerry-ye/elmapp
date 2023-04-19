3d6ce0a589e416ff348b8925594586802013cb4d
YE Zihang <jerryzihye@outlook.com>
2023-04-19 14:22:37 +0900

    Test failed tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,23 +1,49 @@
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Main where
 
 import Test.QuickCheck
 import ElmlensProp
 import Elmlens
 import Apps
+import Benchmark
+import System.Random
+import Test.QuickCheck.Instances
 
 instance MaskedUpdateStructure IntU where
   type Mask IntU = ()
   mask _ _ = ()
   eqv _ _ = (==)
+
+instance Eq a => MaskedUpdateStructure (RepU a) where
+  type Mask (RepU a) = ()
+  mask _ _ = ()
+  eqv _ _ = (==)
   
+instance (Monoid a, Eq a) => MaskedUpdateStructure (UnitU a) where
+  type Mask (UnitU a) = ()
+  mask _ _ = ()
+  eqv _ _ = (==)
+
+instance MaskedUpdateStructure LabelU where
+  type Mask LabelU = ()
+  mask _ _ = ()
+  eqv _ _ = (==)
+
+instance Arbitrary StdGen where
+  arbitrary = fmap mkStdGen chooseAny
 
 proj2L' :: ULens (ProdU IntU IntU) IntU
 proj2L' = proj2L 0
 
+testULens lens = do
+  quickCheck $ consistencyProp lens
+  quickCheck $ createProp lens
+  quickCheck $ translateEmptyProp lens
+  quickCheck $ translateCombineProp lens
+
+testElmApp (ElmApp lens _) = testULens lens
+
 main :: IO ()
 main = do
-  quickCheck $ consistencyProp proj2L'
-  quickCheck $ createProp proj2L'
-  quickCheck $ translateEmptyProp proj2L'
-  quickCheck $ translateCombineProp proj2L'
+  testElmApp buttons
